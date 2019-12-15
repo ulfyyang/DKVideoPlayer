298c6626f86b7e21ae3d6c306855a6159114581e
ulfyyang <ulfy920814>
2019-12-15 15:38:42 +0800

    编写基本视频播放演示

--- custom-sample/src/main/java/com/dueeeke/custom/MainActivity.java
+++ custom-sample/src/main/java/com/dueeeke/custom/MainActivity.java
@@ -4,11 +4,46 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.dueeeke.videocontroller.StandardVideoController;
+import com.dueeeke.videocontroller.component.CompleteView;
+import com.dueeeke.videocontroller.component.ErrorView;
+import com.dueeeke.videocontroller.component.GestureView;
+import com.dueeeke.videocontroller.component.PrepareView;
+import com.dueeeke.videocontroller.component.TitleView;
+import com.dueeeke.videocontroller.component.VodControlView;
+import com.dueeeke.videoplayer.player.VideoView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends AppCompatActivity {
+    private VideoView videoVV;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoVV = findViewById(R.id.videoVV);
+        setupVideoController();
+
+        videoVV.setUrl("http://34.92.158.191:8080/test-hd.mp4");
+        videoVV.start();
+    }
+
+    private void setupVideoController() {
+        StandardVideoController controller = new StandardVideoController(this);
+        //根据屏幕方向自动进入/退出全屏
+        controller.setEnableOrientation(true);
+        PrepareView prepareView = new PrepareView(this);//准备播放界面
+        controller.addControlComponent(prepareView);
+        controller.addControlComponent(new CompleteView(this));//自动完成播放界面
+        controller.addControlComponent(new ErrorView(this));//错误界面
+        TitleView titleView = new TitleView(this);//标题栏
+        controller.addControlComponent(titleView);
+        VodControlView vodControlView = new VodControlView(this);//点播控制条
+        //是否显示底部进度条。默认显示
+//                vodControlView.showBottomProgress(false);
+        controller.addControlComponent(vodControlView);
+        GestureView gestureControlView = new GestureView(this);//滑动控制视图
+        controller.addControlComponent(gestureControlView);
+        titleView.setTitle("视频标题");
+        videoVV.setVideoController(controller);
     }
 }
