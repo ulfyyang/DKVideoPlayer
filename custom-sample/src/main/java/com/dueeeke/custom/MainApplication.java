cb3c2f5fdae9081243b7921ab52f679096b8a9eb
ulfyyang <ulfy920814>
2019-12-16 16:46:17 +0800

    设置定制演示的播放核心

--- custom-sample/src/main/java/com/dueeeke/custom/MainApplication.java
+++ custom-sample/src/main/java/com/dueeeke/custom/MainApplication.java
@@ -2,11 +2,16 @@ package com.dueeeke.custom;
 
 import android.app.Application;
 
-import com.dueeeke.custom.custom.VideoViewRepository;
+import com.dueeeke.videoplayer.exo.ExoMediaPlayerFactory;
+import com.dueeeke.videoplayer.player.VideoViewConfig;
+import com.dueeeke.videoplayer.player.VideoViewManager;
 
 public class MainApplication extends Application {
 
     @Override public void onCreate() {
         super.onCreate();
+        VideoViewManager.setConfig(VideoViewConfig.newBuilder()
+                .setPlayerFactory(ExoMediaPlayerFactory.create())
+                .build());
     }
 }
